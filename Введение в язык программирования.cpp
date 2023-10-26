dfd9c654013b8347c2c2d582521b64a9955aadde
DarkLordZero19 <100068799+DarkLordZero19@users.noreply.github.com>
2023-10-26 16:44:47 +0300

    Первые задания

---
+++ Введение в язык программирования.cpp
@@ -0,0 +1,155 @@
+﻿//1 Задание
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	int seconds;
+
+	cout << "Введите время в секундах: ";
+	cin >> seconds;
+
+	int hours = seconds / 3600;
+	int minutes = (seconds % 3600) / 60;
+	int remainingSeconds = seconds % 60;
+
+	cout << hours << " часов " << minutes << " минут " << remainingSeconds << " секунд" << endl;
+
+	return 0;
+}
+*/
+
+//2 Задание
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	float number;
+
+	cout << "Введите дробное число: ";
+	cin >> number;
+
+	int integerPart = int(number);
+	float fractionalPart = number - integerPart;
+	int cents = fractionalPart * 100;
+
+	cout << integerPart << " долларов и " << cents << " центов" << endl;
+
+	return 0;
+}
+*/
+
+//3 Задание
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+int days;
+
+cout << "Введите количество дней: ";
+cin >> days;
+
+int weeks = days / 7;
+int remainingDays = days % 7;
+
+cout << weeks << " недель и " << remainingDays << " дней" << endl;
+
+return 0;
+}
+*/
+
+//4 Задание
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+int number;
+
+cout << "Введите 6-значное число: ";
+cin >> number;
+
+// Проверка на 6-значное число
+if(number < 100000 || number > 999999) {
+cout << "Введено не 6-значное число" << endl;
+return 0;
+}
+
+int first3Digits = number / 100;
+int last3Digits = number % 1000;
+
+int sumFirst = first3Digits / 10 + first3Digits % 10 + number % 100 / 10;
+int sumLast = last3Digits / 100 + last3Digits % 100 / 10 + last3Digits % 10;
+
+if(sumFirst == sumLast) {
+cout << "Число счастливое" << endl;
+}
+else {
+cout << "Число не счастливое" << endl;
+}
+
+return 0;
+}
+*/
+
+
+//5 Задание
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+int number;
+
+cout << "Введите 4-значное число: ";
+cin >> number;
+
+// Проверка на 4-значное число
+if(number < 1000 || number > 9999) {
+cout << "Введено не 4-значное число" << endl;
+return 0;
+}
+
+int firstDigit = number/1000;
+int secondDigit = number%1000/100;
+int thirdDigit = number%100/10;
+int fourthDigit = number%10;
+
+int newNumber = fourthDigit1000 + thirdDigit100 + secondDigit*10 + firstDigit;
+
+cout << "Преобразованное число: " << newNumber << endl;
+
+return 0;
+}
+*/
+
+
+//6 Задание
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+int number1, number2, number3, number4, number5, number6, number7;
+int max;
+
+cout << "Введите 7 чисел: ";
+cin >> number1 >> number2 >> number3 >> number4 >> number5 >> number6 >> number7;
+
+max = number1;
+
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+if(number4 > max) max = number4;
+if(number5 > max) max = number5;
+if(number6 > max) max = number6;
+if(number7 > max) max = number7;
+
+cout << "Максимальное число: " << max << endl;
+
+return 0;
+}
+*/
---
+++ Введение в язык программирования2.cpp
@@ -0,0 +1,139 @@
+﻿/*Задание 1
+#include <iostream>
+using namespace std;
+
+int main() {
+char symbol;
+
+cout << "Введите символ: ";
+cin >> symbol;
+
+if((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z')) {
+cout << "Буква" << endl;
+}
+else if(symbol >= '0' && symbol <= '9') {
+cout << "Цифра" << endl;
+}
+else if(symbol == '.' || symbol == ',' || symbol == ';' || symbol == '!' || symbol == '?') {
+cout << "Знак препинания" << endl;
+}
+else {
+cout << "Другой символ" << endl;
+}
+
+return 0;
+}
+*/
+
+/*Задание 2
+#include <iostream>
+using namespace std;
+
+int main() {
+float duration;
+int from, to;
+float cost = 0;
+
+cout << "Длительность разговора (мин): ";
+cin >> duration;
+
+cout << "1 - Билайн, 2 - МТС, 3 - Мегафон: " << endl;
+cout << "Оператор отправления: ";
+cin >> from;
+
+cout << "Оператор получения: ";
+cin >> to;
+
+if(from == 1) {
+// Билайн
+if(to == 1) {
+cost = duration * 1.5;
+}
+else if(to == 2) {
+cost = duration * 2;
+}
+else {
+cost = duration * 3;
+}
+}
+else if(from == 2) {
+// МТС
+if(to == 1) {
+cost = duration * 2;
+}
+else if(to == 2) {
+cost = duration * 1.5;
+}
+else {
+cost = duration * 2.5;
+}
+}
+else {
+// Мегафон
+if(to == 1) {
+cost = duration * 3;
+}
+else if(to == 2) {
+cost = duration * 2.5;
+}
+else {
+cost = duration * 2;
+}
+}
+
+cout << "Стоимость разговора: " << cost << " руб." << endl;
+
+return 0;
+}
+*/
+
+/*Задание 3
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int choice;
+
+cout << "1. Ввести доход и опоздания, посчитать строки кода" << endl;
+cout << "2. Ввести строки кода и доход, посчитать опоздания" << endl;
+cout << "3. Ввести строки кода и опоздания, посчитать зарплату" << endl;
+cout << "Выберите пункт меню: ";
+cin >> choice;
+
+if(choice == 1) {
+int salary, delays;
+cout << "Введиет желаемый доход: ";
+cin >> salary;
+cout << "Введиет количество опозданий: ";
+cin >> delays;
+
+int lines = salary * 2 - delays * 20;
+cout << "Необходимо написать " << lines << " строк кода" << endl;
+
+} else if(choice == 2) {
+
+int lines, salary;
+cout << "Введиет количество строк кода: ";
+cin >> lines;
+cout << "Введиет желаемый доход: ";
+cin >> salary;
+
+int delays = (salary * 2 - lines) / 20;
+cout << "Максимальное количество опозданий: " << delays << endl;
+
+} else {
+int lines, delays;
+cout << "Введиет количество строк кода: ";
+cin >> lines;
+cout << "Введиет количество опозданий: ";
+cin >> delays;
+
+int salary = lines/2 + delays * 20;
+cout << "Зарплата составит: " << salary << " $" << endl;
+
+}
+
+return 0;
+}
+*/
