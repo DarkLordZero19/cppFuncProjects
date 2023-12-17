d36bf67380455c1cccdb17e9d8a864e1c45aa29c
DarkLordZero19 <100068799+DarkLordZero19@users.noreply.github.com>
2023-12-17 11:11:47 +0300

    подсчет

---
+++ подсчет.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <cmath>
+#include <unordered_set>
+using namespace std;
+
+//Реализовать сортировку подсчетом.
+
+void countingSort(int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int* count = new int[max + 1]();
+
+    for (int i = 0; i < size; ++i) {
+        ++count[arr[i]];
+    }
+
+    int idx = 0;
+    for (int i = 0; i <= max; ++i) {
+        while (count[i] > 0) {
+            arr[idx++] = i;
+            --count[i];
+        }
+    }
+
+    delete[] count;
+}
+
+int main() {
+    int arr[] = { 5, 2, 8, 3, 1 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    countingSort(arr, size);
+
+    cout << "Отсортированный массив: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+
+
